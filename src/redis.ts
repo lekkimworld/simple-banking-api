ef2d0c588077692d78535544b79e94ac7707facd
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2021-03-07 20:16:41 +0100

    use TLS url for redis if available

--- src/redis.ts
+++ src/redis.ts
@@ -2,8 +2,15 @@ import { createClient as createRedisClient } from "redis";
 import { URL } from "url";
 
 const redisClient = (function () {
-    const redis_uri = process.env.REDIS_URL ? new URL(process.env.REDIS_URL as string) : undefined;
-    if (process.env.REDIS_URL && redis_uri && redis_uri.protocol!.indexOf("rediss") === 0) {
+    const redis_uri = (function () {
+        if (process.env.REDIS_TLS_URL) {
+            return new URL(process.env.REDIS_TLS_URL as string);
+        } else if (process.env.REDIS_URL) {
+            return new URL(process.env.REDIS_URL as string);
+        }
+        return undefined;
+    })()
+    if (redis_uri && redis_uri.protocol!.indexOf("rediss") === 0) {
         return createRedisClient({
             port: Number.parseInt(redis_uri.port!),
             host: redis_uri.hostname!,
