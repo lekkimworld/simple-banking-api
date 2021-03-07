b397acfe26a353aa43c4295313fd75cf50affbd1
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2021-03-07 23:24:49 +0100

    Better datamodel

--- src/heroku-postbuild.ts
+++ src/heroku-postbuild.ts
@@ -2,45 +2,76 @@ import { config as dotenv_config } from "dotenv";
 dotenv_config();
 import { buildQueryHelper, terminatePool } from "./postgres";
 import moment from "moment";
+import { v4 as uuid } from "uuid";
 
-
-const NUM_ACCOUNTS = 1000;
+const NUM_CUSTOMERS = 1000;
+const MAX_NUM_ACCOUNTS = 6;
 const NUM_TX = 100;
 const START_AMOUNT = 1000000;
 const MAX_AMOUNT = 5000;
 const MIN_AMOUNT = -3000;
 const SECONDS_ADJUST_START = 60 * 60 * 24 * 100;
 const SECONDS_ADJUST_TX = 60 * 60 * 24 * 9;
+const REG_NOS = new Array(10).fill("").map(() => {
+    return `000${Math.ceil(Math.random() * 5000) + 1000}`.substr(-4);
+})
 
 const log = (msg: string) => {
     console.log(`DATALOAD - ${msg}`);
 }
+console.log(REG_NOS);
 
 log("Starting");
 const queryHelper = buildQueryHelper();
-queryHelper.query("select count(*) from account").catch(() => {
+queryHelper.query("select count(*) from account").catch(async () => {
     // create schema
-    return queryHelper.query("create table account(regno integer not null, accountno integer not null, amount float not null default 0.00, txdt timestamp without time zone not null, txid varchar(64));");
+    await queryHelper.query("create table customer (custno varchar(64) not null primary key);")
+    await queryHelper.query("create table account (custno varchar(64) not null, regno integer not null, accountno bigint not null, amount float not null default 0.00, lasttx timestamp without time zone);")
+    await queryHelper.query("alter table account add primary key (regno, accountno);");
+    await queryHelper.query("alter table account add foreign key (custno) references customer(custno) on delete cascade;");
+    await queryHelper.query("create table tx (regno integer not null, accountno bigint not null, before float not null default 0.00, delta float not null default 0.00, after float not null default 0.00, txdt timestamp without time zone not null, txid varchar(64));");
+    await queryHelper.query("alter table tx add foreign key (regno, accountno) references account(regno, accountno) on delete cascade;");
 }).then(() => {
-    return queryHelper.query("delete from account;");
-}).then(async () => {
-    for (let i = 0; i < NUM_ACCOUNTS; i++) {
-        const regno = `000${Math.ceil(Math.random() * 5000) + 1000}`.substr(-4);
-        const accountno = `000${Math.ceil(10000000000 * Math.random())}`.substr(-10);
-        log(`Generated account - ${regno} ${accountno}`);
+    queryHelper.query("delete from account;");
 
-        let amount = Math.ceil(Math.random() * START_AMOUNT) / 100;
-        let txdt = moment().utc().subtract(Math.random() * SECONDS_ADJUST_START, "seconds");
-        const num_tx = Math.ceil(Math.random() * NUM_TX);
-        log(`Inserting ${num_tx} transactions starting with amount ${amount} at ${txdt.toISOString()}`);
-        for (let k = 0; k < num_tx; k++) {
-            const delta_amount = (Math.round(Math.random() * (MAX_AMOUNT - MIN_AMOUNT) + MIN_AMOUNT) * 100) / 100;
-            amount += delta_amount;
-            await queryHelper.query("insert into account (regno, accountno, amount, txdt) values ($1, $2, $3, $4)", regno, accountno, amount, txdt);
-            txdt.add(Math.random() * SECONDS_ADJUST_TX, "seconds");
+
+}).then(async () => {
+    for (let i = 0; i < NUM_CUSTOMERS; i++) {
+        // create customer no
+        const custno = uuid();
+        await queryHelper.query("insert into customer (custno) values ($1)", custno);
+        log(`Created customer: ${custno}`);
+
+        for (let j = 0; j < Math.floor(Math.random() * MAX_NUM_ACCOUNTS); j++) {
+            // get reg no and account no
+            const regno = REG_NOS[Math.floor(Math.random() * 10)];
+            const accountno = `000${Math.ceil(10000000000 * Math.random())}`.substr(-10);
+
+            // create account
+            let amount = Math.ceil(Math.random() * START_AMOUNT) / 100;
+            let txdt = moment().utc().subtract(Math.random() * SECONDS_ADJUST_START, "seconds");
+            await queryHelper.query("insert into account (regno, accountno, amount, lasttx, custno) values ($1, $2, $3, $4, $5)", regno, accountno, amount, txdt, custno);
+            log(`Generating account - ${regno} ${accountno} with amount ${amount}`);
+
+            const num_tx = Math.ceil(Math.random() * NUM_TX);
+            log(`Inserting ${num_tx} transactions starting with amount ${amount} at ${txdt.toISOString()}`);
+            for (let k = 0; k < num_tx; k++) {
+                const delta_amount = (Math.round(Math.random() * (MAX_AMOUNT - MIN_AMOUNT) + MIN_AMOUNT) * 100) / 100;
+                const before_amount = amount;
+                amount += delta_amount;
+                await queryHelper.query("insert into tx (regno, accountno, before, delta, after, txdt) values ($1, $2, $3, $4, $5, $6)", regno, accountno, before_amount, delta_amount, amount, txdt);
+                txdt.add(Math.random() * SECONDS_ADJUST_TX, "seconds");
+
+                // ensure we do not go part current dt
+                if (txdt.isAfter(moment.now())) break;
+            }
+
+            // update amount
+            await queryHelper.query("update account set amount=$1, lasttx=$2 where regno=$3 and accountno=$4", amount, txdt, regno, accountno);
         }
     }
-}).then((() => {
+}).then(() => {
     log("Done");
+}).then(() => {
     terminatePool();
-}))
+})
